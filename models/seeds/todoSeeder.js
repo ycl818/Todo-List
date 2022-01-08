71dbfab3af74ba8f3169c12181807a97c17d7b05
ycl818 <kevinyichienlee@gmail.com>
2022-01-08 18:51:02 -0500

    initial commit

---
+++ models/seeds/todoSeeder.js
@@ -0,0 +1,10 @@
+const Todo = require('../todo')
+const db = require('../../config/mongoose')
+
+db.once('open', () => {
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: 'name-' + i })
+  }
+
+  console.log('done')
+})
