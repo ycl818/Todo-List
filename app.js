c825f9604e05373edc78ef139758f0c9112ea5e2
ycl818 <kevinyichienlee@gmail.com>
2022-01-21 22:43:41 -0500

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-const express = require('express')
-const session = require('express-session')
-const exphbs = require('express-handlebars')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
-const routes = require('./routes')
+const routes = require("./routes");
 
-const usePassport = require('./config/passport')
-require('./config/mongoose')
+const usePassport = require("./config/passport");
+require("./config/mongoose");
 
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
-app.set('view engine', 'hbs')
-app.use(session({
-  secret:'ThisIsMySecret',
-  resave: false,
-  saveUninitialized: true
-}))
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.use(
+  session({
+    secret: "ThisIsMySecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-usePassport(app)
-app.use(routes)
+usePassport(app);
+
+app.use((req, res, next) => {
+  console.log(req.user);
+  res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.user = req.user;
+  next();
+});
+app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(`App is running on http://localhost:${PORT}`)
-})
+  console.log(`App is running on http://localhost:${PORT}`);
+});
