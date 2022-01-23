78a26259f50ecbb5955c194245f7847acc4e69d3
ycl818 <kevinyichienlee@gmail.com>
2022-01-23 00:41:51 -0500

    feat: add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,34 +1,43 @@
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
 
-const User = require('../models/user')
+const User = require("../models/user");
 
-module.exports = app => {
-  app.use(passport.initialize())
-  app.use(passport.session())
+module.exports = (app) => {
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-  passport.use(new LocalStrategy({ usernameField: 'email'}, (email, password, done) => {
-    User.findOne({ email })
-    .then(user => {
-      if (!user) {
-        return done(null, false, {message: 'That email is not registered!'})
-      }
-      if (user.password !== password) {
-        return done(null, false, {message: 'Email or Password incorrect'})
-      }
-      return done(null, user)
+  passport.use(
+    new LocalStrategy({ usernameField: "email" }, (email, password, done) => {
+      User.findOne({ email })
+        .then((user) => {
+          if (!user) {
+            return done(null, false, {
+              message: "That email is not registered!",
+            });
+          }
+          return bcrypt.compare(password, user.password).then((isMatch) => {
+            if (!isMatch) {
+              return done(null, false, {
+                message: "Email or Password incorrect",
+              });
+            }
+            return done(null, user);
+          });
+        })
+        .catch((err) => done(err, false));
     })
-    .catch(err => done(err, false))
-  }))
+  );
 
   passport.serializeUser((user, done) => {
-    done(null, user.id)
-  })
+    done(null, user.id);
+  });
 
   passport.deserializeUser((id, done) => {
     User.findById(id)
-    .lean()
-    .then(user => done(null, user))
-    .catch(err => done(err, null))
-  })
-}
+      .lean()
+      .then((user) => done(null, user))
+      .catch((err) => done(err, null));
+  });
+};
